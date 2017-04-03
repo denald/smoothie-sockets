dc33420ac4566646a8005014fe5d43f43a91cf79
Denys Lapin <dlapin@amplify.com>
2017-04-03 12:34:59 +0300

    fixes

--- student.py
+++ student.py
@@ -1,7 +1,7 @@
 from flask import session
-from flask_socketio import Namespace, emit, join_room
+from flask_socketio import Namespace, emit
 from flask import request
-import example.clients
+import clients
 
 
 class StudentNameSpace(Namespace):
@@ -9,7 +9,7 @@ class StudentNameSpace(Namespace):
     def on_my_event(self, message):
         session['receive_count'] = session.get('receive_count', 0) + 1
         #Saves student_id and session id to dict
-        example.clients.Students[message['data']] = request.sid
+        clients.Students[message['data']] = request.sid
         emit('my_response',
              {'data': message['data'], 'count': session['receive_count']})
 
@@ -27,10 +27,10 @@ class StudentNameSpace(Namespace):
         print('Client disconnected', request.sid)
 
     def on_ask_help_event(self, message):
-        if len(example.clients.Teacher):
+        if len(clients.Teacher):
             session['receive_count'] = session.get('receive_count', 0) + 1
-            emit('my_response', {'data': 'I"m asking for helpIN ROOM {} '.format(example.clients.Teacher)})
+            emit('my_response', {'data': 'I"m asking for helpIN ROOM {} '.format(clients.Teacher)})
             emit('ask_help_event', {'data': message['data'], 'count': session['receive_count']},
-                 room=example.clients.Teacher[0], namespace='/teacher')
+                 room=clients.Teacher[0], namespace='/teacher')
         else:
             emit('my_response', {'data': 'Teacher is disconnected'})
